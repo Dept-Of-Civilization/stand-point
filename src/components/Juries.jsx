abb0037c79b87a00f403a3f942382ab7fcf406a3
ajmalMundumuzhi <muhajmal73@gmail.com>
2025-12-25 13:11:19 +0530

    feat: Timeline download link added

---
+++ src/components/Juries.jsx
@@ -0,0 +1,13 @@
+expoort default Juries() {
+    return (
+        <section className="flex items-center justify-center max-h-screen py-20">
+            <div className="grid grid-cols-4 md:grid-cols-12">
+                <article className="col-span-4">
+                    <h2 className="text-secondary tracking-wider font-extrabold text-6xl font-mono mb-16">
+                        JURIES
+                    </h2>
+                </article>
+            </div>
+        </section>
+    )
+}
--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -1,3 +1,13 @@
 export default function TimeLine() {
-    return
+    return (
+        <section className="flex items-center justify-center py-20">
+            <a 
+                href="/public/images/timeline pdf.pdf"
+                target="_blank"
+                className="text-primary font-bold ext-xl md:text-3xl bg-secondary w-64 md:w-90 text-center p-4 md:p-6 rounded-full shadow-2xl shadow-black hover:scale-105 transition-transform"    
+            >
+                Download Timeline
+            </a>
+        </section>
+    )
 }
